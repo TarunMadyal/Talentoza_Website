ff8b6cdeaef4da24866ca5b5abc9f30f81e4b715
TarunMadyal <tarunmadyal@gmail.com>
2025-06-04 23:53:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", () => {
+    window.addEventListener("scroll", reveal);
+
+    function reveal() {
+        const reveals = document.querySelectorAll(".section, .about‐card, .service-card");
+
+        for (let i = 0; i < reveals.length; i++) {
+            const windowHeight = window.innerHeight;
+            const elementTop = reveals[i].getBoundingClientRect().top;
+            const elementVisible = 150;
+
+            if (elementTop < windowHeight - elementVisible) {
+                reveals[i].classList.add("active");
+            } else {
+                reveals[i].classList.remove("active");
+            }
+        }
+    }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    window.addEventListener("scroll", reveal);
+  
+    function reveal() {
+      const reveals = document.querySelectorAll(
+        ".section, .about‐card, .service-card, .timeline-item, .industry-card"
+      );
+  
+      for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 150;
+  
+        if (elementTop < windowHeight - elementVisible) {
+          reveals[i].classList.add("active");
+        } else {
+          reveals[i].classList.remove("active");
+        }
+      }
+    }
+  });
+  document.addEventListener("DOMContentLoaded", () => {
+    window.addEventListener("scroll", reveal);
+  
+    function reveal() {
+      const reveals = document.querySelectorAll(
+        ".section, .about-card, .service-card, .timeline-item, .industry-card, .contact-form"
+      );
+  
+      for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 150;
+  
+        if (elementTop < windowHeight - elementVisible) {
+          reveals[i].classList.add("active");
+        } else {
+          reveals[i].classList.remove("active");
+        }
+      }
+    }
+  });
+  
